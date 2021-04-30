2aca449fa3f62e634550a49768f9b32c5ba8e343
Dhairya Sharma <dhairyasharma@Dhairyas-MacBook-Air.local>
2021-04-30 21:16:47 +0530

    Route added.

---
+++ src/Configurations/Routes.js
@@ -0,0 +1,10 @@
+import Home from '../Screens/Home';
+import { Router, Route } from "react-router";
+
+const routes = (
+    <Route path="/" component={Home}>
+        
+    </Route>
+);
+
+export default routes;
