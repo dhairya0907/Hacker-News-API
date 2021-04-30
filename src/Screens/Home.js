66ca227761ce0818974119db77d8da8a0b89a895
Dhairya Sharma <dhairyasharma@Dhairyas-MacBook-Air.local>
2021-04-30 22:50:26 +0530

    Home page hadder and new and past button added.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
-import { browserHistory } from 'react-router';
-
+import { browserHistory } from "react-router";
+import "../CSS/Home.css";
 
 export default class App extends Component {
-  onClick() {
-    browserHistory.push("/Test");
-  }
+  state = {
+    newSelected: true,
+  };
+
+  async componentDidMount() {}
+
   render() {
-    return <button onClick={this.onClick.bind(this)}>Add New user</button>;
+    return (
+      <div>
+        <header className="Home-header">
+          <div class="Title-header">
+            <div class="Title-style">
+              <text class="Title-text1">HACKER</text>
+              <text class="Title-text2">NEWS</text>
+              <text class="Title-text1">.</text>
+            </div>
+          </div>
+          <div
+            class="Top-buttons"
+            style={{
+              backgroundColor: this.state.newSelected ? "#FBC91B" : "#F2F2F2",
+              left: 16,
+            }}
+            onClick={() => this.setState({ newSelected: true })}
+          >
+            <text class="Top-buttons-text">New</text>
+          </div>
+          <div
+            class="Top-buttons"
+            style={{
+              backgroundColor: !this.state.newSelected ? "#FBC91B" : "#F2F2F2",
+              left: 110,
+            }}
+            onClick={() => this.setState({ newSelected: false })}
+          >
+            <text class="Top-buttons-text">Past</text>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
 import routes from "../src/Configurations/Routes";
+import './assets/Open_Sans/OpenSans-Regular.ttf'
+import './assets/Open_Sans/OpenSans-Bold.ttf'
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes} />,
