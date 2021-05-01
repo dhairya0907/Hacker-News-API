127b0eea64852370e8085a5cb14191c30b2fc44a
Dhairya Sharma <dhairyasharma@Dhairyas-MacBook-Air.local>
2021-05-01 07:29:08 +0530

    Implemented Flatlist.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
 import { browserHistory } from "react-router";
+import FlatList from "flatlist-react";
+
 import "../CSS/Home.css";
+
+const people = [
+  { firstName: "Elson", lastName: "Correia", info: { age: 24 } },
+  { firstName: "John", lastName: "Doe", info: { age: 18 } },
+  { firstName: "Elson", lastName: "Correia", info: { age: 24 } },
+  { firstName: "John", lastName: "Doe", info: { age: 18 } },
+  { firstName: "Elson", lastName: "Correia", info: { age: 24 } },
+  { firstName: "John", lastName: "Doe", info: { age: 18 } },
+  { firstName: "Elson", lastName: "Correia", info: { age: 24 } },
+  { firstName: "John", lastName: "Doe", info: { age: 18 } },
+ 
+];
 
 export default class App extends Component {
   state = {
     newSelected: true,
   };
+  disableScrolling() {
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function () {
+      window.scrollTo(x-10, y-10);
+    };
+  }
 
-  async componentDidMount() {}
+  async componentDidMount() {
+    this.disableScrolling();
+  }
+
+  renderPerson = (person, idx) => {
+    return (
+      <li>
+        <div class="List-box ">
+          <text style={{ color: "black" }}>
+            {person.firstName} {person.lastName}
+          </text>
+        </div>
+      </li>
+    );
+  };
 
   render() {
     return (
-      <div>
-        <header className="Home-header">
+      <div class='Main-div'>
+        <header class="Home-header">
           <div class="Title-header">
             <div class="Title-style">
               <text class="Title-text1">HACKER</text>
@@ -40,12 +75,20 @@ export default class App extends Component {
           >
             <text class="Top-buttons-text">Past</text>
           </div>
+          <div class="List-outer-div" style={{ overflowY: "scroll" }}>
+            <FlatList
+              list={people}
+              renderItem={this.renderPerson}
+              renderWhenEmpty={() => <div>List is empty!</div>}
+            />
+
+            </div>
           <button
-            class="Load-box"
-            onClick={() => this.setState({ newSelected: false })}
-          >
-            <text class="Load-text">Load More</text>
-          </button>
+              class="Load-box"
+              onClick={() => this.setState({ newSelected: !this.state.newSelected })}
+            >
+              <text class="Load-text">Load More</text>
+            </button>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Router, browserHistory } from "react-router";
 import routes from "../src/Configurations/Routes";
 import './assets/Open_Sans/OpenSans-Regular.ttf'
-import './assets/Open_Sans/OpenSans-Bold.ttf'
+import './assets/Open_Sans/OpenSans-ExtraBold.ttf'
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes} />,
